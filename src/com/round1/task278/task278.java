a60a2d6e9fd4839d458563060e1b159df54b8e17
tajbe <tajbe123@gmail.com>
2018-02-07 13:13:18 +0300

    create function check evoluton

--- src/com/round1/task278/task278.java
+++ src/com/round1/task278/task278.java
@@ -1,5 +1,42 @@
 package com.round1.task278;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class task278 {
-    
+    public static void main(String[] args) {
+        BufferedReader readBuff;
+        File inputfile = new File("src\\com\\round1\\task278\\input.txt");
+        try (BufferedWriter writeBuff = new BufferedWriter(new FileWriter("src\\com\\round1\\task278\\output.txt", false))) {
+            if (inputfile.exists()) try {
+                if ((inputfile.lastModified() / 1024) >= 256) {
+                    readBuff = new BufferedReader(new FileReader("src\\com\\round1\\task278\\input.txt"));
+                    String dNKSequence = readBuff.readLine();
+                    String evolutionDNKSequence = readBuff.readLine();
+                    writeBuff.write(String.valueOf(CheckEvolution(dNKSequence, evolutionDNKSequence)));
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static boolean CheckEvolution(String dNKSequence, String evolutionDNKSequence) {
+        ArrayList<String> buffArray = new ArrayList<>();
+        Integer startOfSubstring = 0;
+        for (int j = 0; j < dNKSequence.length(); j++) {
+            for (int i = startOfSubstring; i < evolutionDNKSequence.length(); i++) {
+                if (dNKSequence.charAt(j) == evolutionDNKSequence.charAt(i)) {
+                    buffArray.add(evolutionDNKSequence.substring(startOfSubstring, i + 1));
+                    startOfSubstring = i;
+                    break;
+                }
+            }
+        }
+        return buffArray.size() == dNKSequence.length();
+    }
 }
